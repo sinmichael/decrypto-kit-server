abc77e44620356bdc8b4482e03ff5213094e1123
Sean Michael <seanmichaelcvasquez@gmail.com>
2023-02-05 19:18:32 +0800

    Revert to built-in Logger

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { WinstonModule } from 'nest-winston';
-import * as winston from 'winston';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const transportConsole = new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize({ all: true }),
-      winston.format.timestamp({
-        format: 'MMM-DD-YYYY HH:mm:ss',
-      }),
-      winston.format.printf(
-        (info) => `${[info.timestamp]}: [${info.level}]: ${info.message}`,
-      ),
-    ),
-  });
-
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-    logger: WinstonModule.createLogger({
-      transports: [transportConsole],
-    }),
-  });
-  app.setGlobalPrefix('api');
-
   const port = process.env.PORT || 3000;
-
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(port);
 }
 bootstrap();
+
+// const transportConsole = new winston.transports.Console({
+//   format: winston.format.combine(
+//     winston.format.colorize({ all: true }),
+//     winston.format.timestamp({
+//       format: 'MMM-DD-YYYY HH:mm:ss',
+//     }),
+//     winston.format.printf(
+//       (info) => `${[info.timestamp]}: [${info.level}]: ${info.message}`,
+//     ),
+//   ),
+// });
+
+// const app = await NestFactory.create(AppModule, {
+//   cors: true,
+//   logger: WinstonModule.createLogger({
+//     transports: [transportConsole],
+//   }),
+// });
