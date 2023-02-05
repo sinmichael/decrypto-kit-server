2aa17e2b91aa0b72e31acc3c4fdfa7fd9b42a7f0
Sean Michael <seanmichaelcvasquez@gmail.com>
2023-02-05 19:07:24 +0800

    Add Winston logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const transportConsole = new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize({ all: true }),
+      winston.format.timestamp({
+        format: 'MMM-DD-YYYY HH:mm:ss',
+      }),
+      winston.format.printf(
+        (info) => `${[info.timestamp]}: [${info.level}]: ${info.message}`,
+      ),
+    ),
+  });
+
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: WinstonModule.createLogger({
+      transports: [transportConsole],
+    }),
+  });
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
