f0f9ee9d79fae13d62e23f9eb726f7f8b677b769
Sean Michael <seanmichaelcvasquez@gmail.com>
2023-02-05 19:12:32 +0800

    Add HttpModule

--- src/switches/switches.module.ts
+++ src/switches/switches.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { SwitchesService } from './switches.service';
 import { SwitchesController } from './switches.controller';
 
 @Module({
+  imports: [HttpModule],
   controllers: [SwitchesController],
-  providers: [SwitchesService]
+  providers: [SwitchesService],
 })
 export class SwitchesModule {}
