16b5e16d91e7cf93b181168fe126239990f2a053
Sean Michael <seanmichaelcvasquez@gmail.com>
2023-02-05 19:18:46 +0800

    Add Switch

--- src/switches/switches.module.ts
+++ src/switches/switches.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { SwitchesService } from './switches.service';
 import { SwitchesController } from './switches.controller';
+import { Switch } from './entities/switch.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HttpModule],
+  imports: [TypeOrmModule.forFeature([Switch]), HttpModule],
   controllers: [SwitchesController],
   providers: [SwitchesService],
 })
